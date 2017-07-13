14dbc5438c56086265128896e8deb5b3c0f7078d
gonzalo.sestopal <gonzalo.sestopal@corbisglobal.com>
2017-07-13 16:25:48 -0300

    login actualizado

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+  
+  clicked(email, password) {
+      if (email.value == '123' || email.value == 'juanjose@eudoxa.mx' || email.value == 'exponentialdemo@singularityu.org') {
+      this.router.navigateByUrl('/usuario');
+    } else {
+      email.classList.add('invalid');
+      var form = document.getElementById("validation-form");
+      form.style.visibility = 'visible';
+      form.style.opacity = "1";
+    }
+  } 
 
 }
--- src/app/usuariohome/usuariohome.component.ts
+++ src/app/usuariohome/usuariohome.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-usuariohome',
   templateUrl: './usuariohome.component.html',
@@ -10,6 +10,7 @@ export class UsuariohomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $('[data-toggle="tooltip"]').tooltip();
   }
 
 }
